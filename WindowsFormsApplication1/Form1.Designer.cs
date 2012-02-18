4f025514844b7d848ec049efaee29969d186fe76
Ruben <yosoyruben@gmail.com>
2012-02-18 11:10:06 +0100

    Ruben mola

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -46,9 +46,9 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(102, 95);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.Size = new System.Drawing.Size(55, 17);
             this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "Cosas";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
