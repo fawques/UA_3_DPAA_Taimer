d73ca875425f5af354a6335f844cf3dcecfa478b
Ruben <yosoyruben@gmail.com>
2012-02-23 21:23:07 +0100

    Empezado a crear el apartado cliente

--- TaimerGUI/ClientForm.Designer.cs
+++ TaimerGUI/ClientForm.Designer.cs
@@ -23,70 +23,194 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClientForm));
+            this.pnlMenu = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.pnlTittle = new System.Windows.Forms.Panel();
+            this.iconNotifClient = new System.Windows.Forms.NotifyIcon(this.components);
+            this.ctxtMenuTray = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.btClose = new System.Windows.Forms.Button();
+            this.btMinimize = new System.Windows.Forms.Button();
+            this.btMaximize = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pnlMenu.SuspendLayout();
+            this.pnlTittle.SuspendLayout();
+            this.ctxtMenuTray.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // pnlMenu
             // 
-            this.panel1.Controls.Add(this.button2);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(274, 576);
-            this.panel1.TabIndex = 2;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(0, 148);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(272, 92);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Panic Button";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.pnlMenu.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.pnlMenu.BackColor = System.Drawing.Color.Khaki;
+            this.pnlMenu.Controls.Add(this.button1);
+            this.pnlMenu.Location = new System.Drawing.Point(1, 83);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(165, 573);
+            this.pnlMenu.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(11, 17);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(272, 142);
+            this.button1.Size = new System.Drawing.Size(132, 73);
             this.button1.TabIndex = 0;
             this.button1.Text = "Horarios";
             this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pnlTittle
+            // 
+            this.pnlTittle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlTittle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.pnlTittle.Controls.Add(this.panel3);
+            this.pnlTittle.Location = new System.Drawing.Point(1, 1);
+            this.pnlTittle.Name = "pnlTittle";
+            this.pnlTittle.Size = new System.Drawing.Size(935, 83);
+            this.pnlTittle.TabIndex = 4;
+            // 
+            // iconNotifClient
+            // 
+            this.iconNotifClient.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.iconNotifClient.BalloonTipText = "Taimer horario";
+            this.iconNotifClient.ContextMenuStrip = this.ctxtMenuTray;
+            this.iconNotifClient.Icon = ((System.Drawing.Icon)(resources.GetObject("iconNotifClient.Icon")));
+            this.iconNotifClient.Text = "notifyIcon1";
+            this.iconNotifClient.Visible = true;
+            this.iconNotifClient.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.iconNotifClient_MouseDoubleClick);
+            // 
+            // ctxtMenuTray
+            // 
+            this.ctxtMenuTray.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.ctxtMenuTray.Name = "ctxtMenuTray";
+            this.ctxtMenuTray.Size = new System.Drawing.Size(143, 48);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(142, 22);
+            this.toolStripMenuItem1.Text = "Cerrar sesion";
+            this.toolStripMenuItem1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripMenuItem1.Click += new System.EventHandler(this.toolStripMenuItem1_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(142, 22);
+            this.toolStripMenuItem2.Text = "Salir";
+            this.toolStripMenuItem2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.toolStripMenuItem2.TextDirection = System.Windows.Forms.ToolStripTextDirection.Horizontal;
+            this.toolStripMenuItem2.Click += new System.EventHandler(this.toolStripMenuItem2_Click);
+            // 
+            // btClose
+            // 
+            this.btClose.BackColor = System.Drawing.Color.Transparent;
+            this.btClose.FlatAppearance.BorderSize = 0;
+            this.btClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btClose.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btClose.ForeColor = System.Drawing.Color.Maroon;
+            this.btClose.Location = new System.Drawing.Point(80, -3);
+            this.btClose.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(31, 27);
+            this.btClose.TabIndex = 9;
+            this.btClose.Text = "X";
+            this.btClose.UseVisualStyleBackColor = false;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click);
+            // 
+            // btMinimize
+            // 
+            this.btMinimize.BackColor = System.Drawing.Color.Transparent;
+            this.btMinimize.FlatAppearance.BorderSize = 0;
+            this.btMinimize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btMinimize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btMinimize.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMinimize.ForeColor = System.Drawing.Color.Maroon;
+            this.btMinimize.Location = new System.Drawing.Point(4, -4);
+            this.btMinimize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btMinimize.Name = "btMinimize";
+            this.btMinimize.Size = new System.Drawing.Size(31, 27);
+            this.btMinimize.TabIndex = 10;
+            this.btMinimize.Text = "__";
+            this.btMinimize.UseVisualStyleBackColor = false;
+            this.btMinimize.Click += new System.EventHandler(this.btMinimize_Click);
+            // 
+            // btMaximize
+            // 
+            this.btMaximize.BackColor = System.Drawing.Color.Transparent;
+            this.btMaximize.FlatAppearance.BorderSize = 0;
+            this.btMaximize.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btMaximize.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btMaximize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btMaximize.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMaximize.ForeColor = System.Drawing.Color.Maroon;
+            this.btMaximize.Location = new System.Drawing.Point(43, 0);
+            this.btMaximize.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btMaximize.Name = "btMaximize";
+            this.btMaximize.Size = new System.Drawing.Size(31, 27);
+            this.btMaximize.TabIndex = 11;
+            this.btMaximize.Text = "O";
+            this.btMaximize.UseVisualStyleBackColor = false;
+            this.btMaximize.Click += new System.EventHandler(this.btMaximize_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.btMaximize);
+            this.panel3.Controls.Add(this.btClose);
+            this.panel3.Controls.Add(this.btMinimize);
+            this.panel3.Location = new System.Drawing.Point(825, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(107, 36);
+            this.panel3.TabIndex = 7;
             // 
             // ClientForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(1206, 576);
-            this.ControlBox = false;
-            this.Controls.Add(this.panel1);
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(938, 657);
+            this.Controls.Add(this.pnlTittle);
+            this.Controls.Add(this.pnlMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.IsMdiContainer = true;
-            this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "ClientForm";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ClientForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ClientForm_FormClosing);
             this.Load += new System.EventHandler(this.ClientForm_Load);
-            this.panel1.ResumeLayout(false);
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlTittle.ResumeLayout(false);
+            this.ctxtMenuTray.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlMenu;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel pnlTittle;
+        private System.Windows.Forms.NotifyIcon iconNotifClient;
+        private System.Windows.Forms.ContextMenuStrip ctxtMenuTray;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.Button btMaximize;
+        private System.Windows.Forms.Button btMinimize;
+        private System.Windows.Forms.Panel panel3;
+
 
     }
 }
--- TaimerGUI/ClientForm.cs
+++ TaimerGUI/ClientForm.cs
@@ -9,11 +9,10 @@ using System.Windows.Forms;
 
 namespace TaimerGUI {
     public partial class ClientForm : Form {
+
+        bool exitOfAplication;
         public ClientForm() {
             InitializeComponent();
-            Bienvenida init = new Bienvenida();
-            init.MdiParent = this;
-            init.Show();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -29,7 +28,80 @@ namespace TaimerGUI {
 
         private void ClientForm_Load(object sender, EventArgs e)
         {
-            
+            Bienvenida init = new Bienvenida();
+            init.MdiParent = this;
+            init.Show();
+
+            exitOfAplication = false;
+        }
+
+        private void iconNotifClient_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void ClientForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!exitOfAplication)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+            else
+            {
+                Application.Exit();
+            }
+                
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            exitOfAplication = true;
+            Program.loginForm.Show();
+            Close();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            exitOfAplication = true;
+            Close();
+        }
+
+        private void btClose_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("¿Seguro que desea salir?",
+                "¿Salir?",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2) == DialogResult.OK)
+            {
+                exitOfAplication = true;
+                Close();
+            }
+        }
+
+        private void btMinimize_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btMaximize_Click(object sender, EventArgs e)
+        {
+            //Desactivamos el borderless para hacer el maxmizado normal, si no se pone a pantalla completa
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            if (this.WindowState != FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Normal;
+
+            }
+            // desactivamos el maximizado para que deje de mostrar el borde
+            this.FormBorderStyle = FormBorderStyle.None;
         }
 
         
