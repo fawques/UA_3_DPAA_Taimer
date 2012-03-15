9f314f5ed38e911e923853555fa9a5878842116f
Victor Guzman <fawques7@hotmail.com>
2012-03-15 18:52:09 +0100

    [Horario] Modificado el constructor de copia y anadido metodo Count

--- Taimer/Horario.cs
+++ Taimer/Horario.cs
@@ -48,6 +48,11 @@ namespace Taimer {
             id = h.id;
             nombre = h.nombre;
             usuario = h.usuario;
+
+            for (int i = 0; i < 7; i++)
+            {
+                arrayTurnos[i] = new List<Turno>(h.ArrayTurnos[i]);
+            }
         }
 
         // Ajustar/obtener nombre
@@ -146,105 +151,17 @@ namespace Taimer {
             return false;
         }
 
-        
-        #region ALGORITMO
-
-        // Puntuar un horario según el número de días. Puntuará de 0 a 7, añadirá uno por cada día en el que haya turnos.
-        public static int puntuarDias(Horario horario)
+        public int Count()
         {
-            int puntuacion = 0;
+            int cantidad = 0;
+
             for (int i = 0; i < 7; i++)
             {
-                if (horario.ArrayTurnos[i].Count() > 0)
-                    puntuacion++;
+                cantidad += arrayTurnos[i].Count;
             }
 
-            return puntuacion;
+            return cantidad;
         }
-
-        //Generación de un Horario de forma Voraz
-        public bool generarHorarioVoraz()
-        {
-            User usertest = new User("Aitor Tilla", "12345678X", "bill_gates@hotmail.com", "password", 1, "Ingeniería de Magisterio");
-
-            Turno t1 = new Turno(new Hora(10, 30), new Hora(12, 30), dias.L, "L04");
-            Turno t2 = new Turno(new Hora(11, 30), new Hora(13, 30), dias.L, "L04");
-            Turno t3 = new Turno(new Hora(12, 30), new Hora(14, 30), dias.L, "L04");
-
-            Turno t4 = new Turno(new Hora(10, 30), new Hora(12, 30), dias.X, "L04");
-            Turno t5 = new Turno(new Hora(12, 30), new Hora(14, 30), dias.X, "L04");
-            Turno t6 = new Turno(new Hora(14, 30), new Hora(16, 30), dias.X, "L04");
-
-            Turno t7 = new Turno(new Hora(10, 30), new Hora(14, 30), dias.M, "L04");
-            Turno t8 = new Turno(new Hora(11, 30), new Hora(14, 30), dias.M, "L04");
-            Turno t9 = new Turno(new Hora(12, 30), new Hora(14, 30), dias.M, "L04");
-
-            Actividad_p actP = new Actividad_p("nombre", "descripcion", 6, usertest);
-            actP.AddTurno(t1);
-
-            Actividad_p actP2 = new Actividad_p("nombre", "descripcion", 6, usertest);
-            actP2.AddTurno(t4);
-            actP2.AddTurno(t5);
-            actP2.AddTurno(t6);
-
-            Actividad_a actA = new Actividad_a("nombre2", "descripcion2", 7, "Un profesor");
-            actA.AddTurno(t3);
-            actA.AddTurno(t2);
-
-            Actividad_a actA2 = new Actividad_a("nombre2", "descripcion2", 7, "Otro profesor");
-            actA2.AddTurno(t7);
-            actA2.AddTurno(t8);
-            actA2.AddTurno(t9);
-
-            ArrayList arrayP = new ArrayList();
-            ArrayList arrayA = new ArrayList();
-            arrayP.Add(actP);
-            arrayP.Add(actP2);
-            arrayA.Add(actA);
-            arrayA.Add(actA2);
-
-
-
-            foreach (Actividad_p personal in arrayP)
-            {
-                foreach (Turno item in personal.Turnos)
-                {
-                    try
-                    {
-                        AddTurno(item);
-                    }
-                    catch (NotSupportedException)
-                    {
-                        return false;
-                    }
-                }
-
-            }
-            bool asignado;
-            foreach (Actividad_a asig in arrayA)
-            {
-                asignado = false;
-                foreach (Turno item in asig.Turnos)
-                {
-                    if (asignado)
-                        break;
-                    try
-                    {
-                        AddTurno(item);
-                        asignado = true;
-                    }
-                    catch (NotSupportedException)
-                    {
-                        asignado = false;
-                    }
-                }
-                if (!asignado)
-                    return false;
-            }
-
-            return true;
-        }
-        #endregion
         #endregion
     }
 }
