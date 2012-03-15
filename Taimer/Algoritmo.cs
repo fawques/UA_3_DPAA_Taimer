bacaae68f3f335779099585b81cb15ca4884dd6d
Victor Guzman <fawques7@hotmail.com>
2012-03-15 11:52:27 +0100

    Arreglados los errores al hacer la redistribucion

--- Taimer/Algoritmo.cs
+++ Taimer/Algoritmo.cs
@@ -2,19 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Taimer;
 
 namespace Taimer
 {
-    class Algoritmo
+    public class Algoritmo
     {
         #region PARTE PRIVADA
-        List<Horario> posibles;
-        List<Actividad_a> seleccionadas_a;
-        List<Actividad_p> seleccionadas_p;
+        private List<Horario> posibles;
+        private List<Actividad_a> seleccionadas_a;
+        private List<Actividad_p> seleccionadas_p;
         #endregion
 
         #region PARTE PÚBLICA
+
+        public Algoritmo(List<Actividad_a> sel_a, List<Actividad_p> sel_p)
+        {
+//            seleccionadas_a.
+            seleccionadas_a = sel_a;
+            seleccionadas_p = sel_p;
+            posibles = new List<Horario>();
+        }
 
         // Puntuar un horario según el número de días. Puntuará de 0 a 7, añadirá uno por cada día en el que haya turnos.
         public static int puntuarDias(Horario horario)
@@ -30,65 +37,19 @@ namespace Taimer
         }
 
         //Generación de un Horario de forma Voraz
-        public bool generarHorarioVoraz()
+        public Horario generarHorarioVoraz(string nombre,User user)
         {
-            User usertest = new User("Aitor Tilla", "12345678X", "bill_gates@hotmail.com", "password", 1, "Ingeniería de Magisterio");
-
-            Turno t1 = new Turno(new Hora(10, 30), new Hora(12, 30), dias.L, "L04");
-            Turno t2 = new Turno(new Hora(11, 30), new Hora(13, 30), dias.L, "L04");
-            Turno t3 = new Turno(new Hora(12, 30), new Hora(14, 30), dias.L, "L04");
-
-            Turno t4 = new Turno(new Hora(10, 30), new Hora(12, 30), dias.X, "L04");
-            Turno t5 = new Turno(new Hora(12, 30), new Hora(14, 30), dias.X, "L04");
-            Turno t6 = new Turno(new Hora(14, 30), new Hora(16, 30), dias.X, "L04");
-
-            Turno t7 = new Turno(new Hora(10, 30), new Hora(14, 30), dias.M, "L04");
-            Turno t8 = new Turno(new Hora(11, 30), new Hora(14, 30), dias.M, "L04");
-            Turno t9 = new Turno(new Hora(12, 30), new Hora(14, 30), dias.M, "L04");
-
-            Actividad_p actP = new Actividad_p("nombre", "descripcion", 6, usertest);
-            actP.AddTurno(t1);
-
-            Actividad_p actP2 = new Actividad_p("nombre", "descripcion", 6, usertest);
-            actP2.AddTurno(t4);
-            actP2.AddTurno(t5);
-            actP2.AddTurno(t6);
-
-            Actividad_a actA = new Actividad_a("nombre2", "descripcion2", 7, "Un profesor");
-            actA.AddTurno(t3);
-            actA.AddTurno(t2);
-
-            Actividad_a actA2 = new Actividad_a("nombre2", "descripcion2", 7, "Otro profesor");
-            actA2.AddTurno(t7);
-            actA2.AddTurno(t8);
-            actA2.AddTurno(t9);
-
-            ArrayList arrayP = new ArrayList();
-            ArrayList arrayA = new ArrayList();
-            arrayP.Add(actP);
-            arrayP.Add(actP2);
-            arrayA.Add(actA);
-            arrayA.Add(actA2);
-
-
-
-            foreach (Actividad_p personal in arrayP)
+            Horario h = new Horario(nombre, user);
+            foreach (Actividad_p personal in seleccionadas_p)
             {
                 foreach (Turno item in personal.Turnos)
                 {
-                    try
-                    {
-                        AddTurno(item);
-                    }
-                    catch (NotSupportedException)
-                    {
-                        return false;
-                    }
+                    h.AddTurno(item);
                 }
 
             }
             bool asignado;
-            foreach (Actividad_a asig in arrayA)
+            foreach (Actividad_a asig in seleccionadas_a)
             {
                 asignado = false;
                 foreach (Turno item in asig.Turnos)
@@ -97,7 +58,7 @@ namespace Taimer
                         break;
                     try
                     {
-                        AddTurno(item);
+                        h.AddTurno(item);
                         asignado = true;
                     }
                     catch (NotSupportedException)
@@ -106,10 +67,10 @@ namespace Taimer
                     }
                 }
                 if (!asignado)
-                    return false;
+                    throw new Exception();
             }
 
-            return true;
+            return h;
         }
         #endregion
 
--- algoritmo/Form1.cs
+++ algoritmo/Form1.cs
@@ -20,26 +20,75 @@ namespace algoritmo
 
         private void btGenerar_Click(object sender, EventArgs e)
         {
+            // el user será el 1er elemento de la lista de users de Program
+            User usertest = new User("Aitor Tilla", "12345678X", "bill_gates@hotmail.com", "password", 1, "Ingeniería de Magisterio");
+
+            // los turnos y actividades estarán almacenadas en Program
+            Turno t1 = new Turno(new Hora(10, 30), new Hora(12, 30), dias.L, "L04");
+            Turno t2 = new Turno(new Hora(11, 30), new Hora(13, 30), dias.L, "L04");
+            Turno t3 = new Turno(new Hora(12, 30), new Hora(14, 30), dias.L, "L04");
+
+            Turno t4 = new Turno(new Hora(10, 30), new Hora(12, 30), dias.X, "L04");
+            Turno t5 = new Turno(new Hora(12, 30), new Hora(14, 30), dias.X, "L04");
+            Turno t6 = new Turno(new Hora(14, 30), new Hora(16, 30), dias.X, "L04");
+
+            Turno t7 = new Turno(new Hora(10, 30), new Hora(14, 30), dias.M, "L04");
+            Turno t8 = new Turno(new Hora(11, 30), new Hora(14, 30), dias.M, "L04");
+            Turno t9 = new Turno(new Hora(12, 30), new Hora(14, 30), dias.M, "L04");
+
+            Actividad_p actP = new Actividad_p("nombre", "descripcion", 6, usertest);
+            actP.AddTurno(t1);
+
+            Actividad_p actP2 = new Actividad_p("nombre", "descripcion", 6, usertest);
+            actP2.AddTurno(t4);
+            actP2.AddTurno(t5);
+            actP2.AddTurno(t6);
+
+            Actividad_a actA = new Actividad_a("nombre2", "descripcion2", 7, "Un profesor");
+            actA.AddTurno(t3);
+            actA.AddTurno(t2);
+
+            Actividad_a actA2 = new Actividad_a("nombre2", "descripcion2", 7, "Otro profesor");
+            actA2.AddTurno(t7);
+            actA2.AddTurno(t8);
+            actA2.AddTurno(t9);
+
+            // las listas de actividades seleccionadas vendrán de la interfaz, según lo que seleccione el user
+            List<Actividad_p> listaP = new List<Actividad_p>();
+            List<Actividad_a> listaA = new List<Actividad_a>();
+            listaP.Add(actP);
+            listaP.Add(actP2);
+            listaA.Add(actA);
+            listaA.Add(actA2);
+
+            string nombre_horario = "Hola";
+
+            // se almacenará en Program
+            Algoritmo alg = new Algoritmo(listaA, listaP);
             string texto = "";
-            User u = new User("nombre", "dni", "email", "pass", 5, "titulacion");
-            Horario h = new Horario("prueba", u);
-            if (h.generarHorarioVoraz())
+
+            try
             {
+                Horario h = alg.generarHorarioVoraz(nombre_horario, usertest);
                 foreach (List<Turno> dia in h.ArrayTurnos)
                 {
-                    foreach (Turno item in dia) {
-                       // texto += item.Nombre + " ";
+                    foreach (Turno item in dia)
+                    {
+                         texto += item.Codigo + " ";
                     }
                 }
 
                 MessageBox.Show("Los turnos son: " + texto);
-                int puntDias = Horario.puntuarDias(h);
+                int puntDias = Algoritmo.puntuarDias(h);
                 MessageBox.Show("Tiene una puntuación según los huecos de : " + puntDias);
             }
-            else
+            catch (Exception)
             {
                 MessageBox.Show("Ha habido algún error en el proceso");
             }
+
+
+
         }
     }
 }
