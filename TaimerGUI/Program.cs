743c324401ad55a917159829197489bd475fddcb
Victor Guzman <fawques7@hotmail.com>
2012-03-20 17:16:40 +0100

    Arreglada errata en Estructura.pdf & listas en Program.cs

--- TaimerGUI/Program.cs
+++ TaimerGUI/Program.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using Taimer;
 
 namespace TaimerGUI {
     static class Program {
 
         public static LoginForm loginForm;
+
+        public static List<Actividad_a> asignaturas;
+        public static List<User> usuarios;
+        public static List<Algoritmo> generados;
+        // Estadistica estadistica;
 
         /// <summary>
         /// Punto de entrada principal para la aplicación.
@@ -16,6 +22,10 @@ namespace TaimerGUI {
             
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            asignaturas = new List<Actividad_a>();
+            usuarios = new List<User>();
+            generados = new List<Algoritmo>();
 
             loginForm = new LoginForm();
             Application.Run(loginForm);
