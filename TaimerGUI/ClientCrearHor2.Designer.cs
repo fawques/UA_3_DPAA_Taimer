02444392fce7fb932e35604398186ef0f0e0f6ba
Victor Guzman <fawques7@hotmail.com>
2012-04-11 17:51:16 +0200

    Puesto valor por defecto en la seleccion de restricciones

--- TaimerGUI/ClientCrearHor2.Designer.cs
+++ TaimerGUI/ClientCrearHor2.Designer.cs
@@ -110,13 +110,13 @@
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(95, 17);
             this.radioButton1.TabIndex = 25;
-            this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Menos huecos";
             this.radioButton1.UseVisualStyleBackColor = true;
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
             this.radioButton2.Location = new System.Drawing.Point(465, 139);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(79, 17);
